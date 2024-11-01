d5279c12121fcf63c793db030b75b2840ece5c7c
Ron <rongoods@gmail.com>
2024-11-01 23:31:10 +0100

    add new blog post / save to json

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,10 +10,32 @@ def load_posts():
         return json.load(file)
 
 
+def save_post(new_post):
+    posts = load_posts()
+    posts.append(new_post)
+    with open('blog_posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+
 @app.route('/')
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        new_id = len(load_posts()) + 1
+        new_post = {
+            "id": new_id,
+            "author": request.form['author'],
+            "title": request.form['title'],
+            "content": request.form['content']
+        }
+        save_post(new_post)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
