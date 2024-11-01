8053e7ec4c4ca24317383fbd84e7713a266a95f4
Ron <rongoods@gmail.com>
2024-11-01 17:53:48 +0100

    build structure and display post

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+def load_posts():
+    with open('blog_posts.json', 'r') as file:
+        return json.load(file)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
